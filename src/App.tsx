4b1274680f6e3c8d8f48fbdf5ab82a05ef57908e
saramar23 <sara.maragoni@yahoo.com>
2025-09-10 19:48:52 -0700

    h1 changed

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,12 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        
+      <h1>Hello!</h1>
+      <div className="card">        
       </div>
       
     </>
